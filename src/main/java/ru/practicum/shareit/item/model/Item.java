a5a0b7f53009bd01afa53cdff0a6922993270a56
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-10-05 17:53:49 +1000

    Переработал модель Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,15 +1,30 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
+import ru.practicum.shareit.user.model.User;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
 public class Item {
-    private Long id;
+    // Уникальный идентификатор вещи
+    private long id;
+
+    // Краткое название вещи
     private String name;
+
+    // Развёрнутое описание вещи
     private String description;
-    private boolean availableForRent;
+
+    // Статус о том, доступна или нет вещь для аренды
+    private boolean available;
+
+    // Владелец вещи
+    private User owner;
+
+    // Если вещь была создана по запросу другого пользователя, то в этом поле будет храниться ссылка на соответствующий запрос
+    private Request request;
 }
+
 
