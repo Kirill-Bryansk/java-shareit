ecddcedb1b79c837b4081bbadf891b9b662fdebe
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-11-11 13:51:59 +1000

    Поправил контроллер item

--- server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -32,6 +32,7 @@ public class ItemController {
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @Validated @RequestBody ItemDtoRequestCreate item
     ) {
+        log.info("POST: запрос на создание item от пользователя с ID: {}, данные: {}", userId, item);
         return itemService.create(userId, item);
     }
 
@@ -42,12 +43,14 @@ public class ItemController {
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @Validated @RequestBody ItemDtoRequestUpdate item
     ) {
+        log.info("PATCH: запрос на обновление item с ID: {}, данные: {}, пользователь с ID: {}", itemId, item, userId);
         return itemService.update(itemId, userId, item);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{itemId}")
     public void delete(@PathVariable Long itemId) {
+        log.info("DELETE: запрос на удаление item с ID: {}", itemId);
         itemService.delete(itemId);
     }
 
@@ -57,20 +60,24 @@ public class ItemController {
             @RequestHeader("X-Sharer-User-Id") Long requesterId,
             @PathVariable Long itemId
     ) {
+        log.info("GET: запрос на получение item с ID: {} от пользователя с ID: {}", itemId, requesterId);
         return itemService.getById(requesterId, itemId);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<ItemDtoResponse> getByUserId(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET: запрос на получение списка items для пользователя с ID: {}", userId);
         return itemService.getByUserId(userId);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
     public List<ItemDtoResponse> search(@RequestParam String text) {
-        if (!StringUtils.hasText(text))
+        log.info("GET: запрос на поиск items по тексту: {}", text);
+        if (!StringUtils.hasText(text)) {
             return List.of();
+        }
         return itemService.search(text);
     }
 
@@ -81,7 +88,7 @@ public class ItemController {
             @PathVariable @Positive Long itemId,
             @Valid @RequestBody CommentDtoRequestCreate comment
     ) {
+        log.info("POST: запрос на добавление комментария к item с ID: {}, от пользователя с ID: {}, данные: {}", itemId, userId, comment);
         return itemService.createComment(userId, itemId, comment);
     }
-
 }
